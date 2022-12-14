ae80697e4563927e63c4ffdc09e2bb9f047e26a7
Jose Schenardie <schenardie@gmail.com>
2022-12-14 11:16:13 +1100

    move folder

--- 1.0.0/Public/Get-WRHistory.ps1
+++ 1.0.0/Public/Get-WRHistory.ps1
@@ -1,25 +1,22 @@
-Function Get-WindowsReleaseInformationHistory {
+Function Get-WRHistory {
   <#
      .Synopsis
-      Parse information from Microsoft Windows Release Information Pages
+      Parse information from Windows Release Information history
     
      .Description
-      This cmdlet parses data form windows release information page and output to a json file.
-    
-     .Parameter OS
-
-     .Example
-    
+      This cmdlet parses data form windows release information history page and output to a json object.
+     .EXAMPLE
+      The function can be run without parameters
     #>
 
   [cmdletbinding()]
   param
   (
-    [Parameter(Mandatory = $False)]$build,
+    [Parameter(Mandatory = $False)]$Build,
     [Parameter(Mandatory = $False)]$KB,
     [Parameter(Mandatory = $False)]$Date,
     [Parameter(Mandatory = $False)]$ServiceOption,
-    [Parameter(Mandatory = $False)]$OS,
+    [Parameter(Mandatory = $True)]$OS,
     [Parameter(Mandatory = $False)]$Version
   )
 
@@ -36,28 +33,29 @@ Function Get-WindowsReleaseInformationHistory {
         if ($table.ParentNode.SelectNodes('strong').InnerText)
         { $ver = ($table.ParentNode.SelectNodes('strong').InnerText).Split(' ')[1] }
         else {
-          $Ver = ($table.ParentNode.FirstChild.InnerText).Split(' ')[1]}
-          if ($ver -like "*End of servicing*") { $EOS = $true }
-          else { $EOS = $False }
-          $data = @()
-          foreach ($tablerow in ($table.Elements('tr') | Select-Object -Skip 1)) {
-
-            $cells = @($tableRow.Elements('td')).InnerText
-            if ($cells[0] -like "*&bull;*") { $cells[0] = $cells[0] -replace "  &bull;  ", " - " }
-            $obj = New-Object -TypeName PSObject
-            $obj | Add-Member -MemberType NoteProperty -Name "Operating System" -Value "Windows $os"
-            $obj | Add-Member -MemberType NoteProperty -Name "Version" -Value $ver
-            $obj | Add-Member -MemberType NoteProperty -Name "Servicing option" -Value $cells[0]
-            $obj | Add-Member -MemberType NoteProperty -Name "Availability date" -Value $cells[1]
-            $obj | Add-Member -MemberType NoteProperty -Name "OS build" -Value $cells[2]
-            $obj | Add-Member -MemberType NoteProperty -Name "Kb article" -Value $cells[3]
-            $obj | Add-Member -MemberType NoteProperty -Name "End of Servicing" -Value $EOS
-            $data += $obj  
-          }
-          $data2 += $data
+          $Ver = ($table.ParentNode.FirstChild.InnerText).Split(' ')[1]
         }
+        if ($ver -like "*End of servicing*") { $EOS = $true }
+        else { $EOS = $False }
+        $data = @()
+        foreach ($tablerow in ($table.Elements('tr') | Select-Object -Skip 1)) {
+
+          $cells = @($tableRow.Elements('td')).InnerText
+          if ($cells[0] -like "*&bull;*") { $cells[0] = $cells[0] -replace "  &bull;  ", " - " }
+          $obj = New-Object -TypeName PSObject
+          $obj | Add-Member -MemberType NoteProperty -Name "Operating System" -Value "Windows $os"
+          $obj | Add-Member -MemberType NoteProperty -Name "Version" -Value $ver
+          $obj | Add-Member -MemberType NoteProperty -Name "Servicing option" -Value $cells[0]
+          $obj | Add-Member -MemberType NoteProperty -Name "Availability date" -Value $cells[1]
+          $obj | Add-Member -MemberType NoteProperty -Name "OS build" -Value $cells[2]
+          $obj | Add-Member -MemberType NoteProperty -Name "Kb article" -Value $cells[3]
+          $obj | Add-Member -MemberType NoteProperty -Name "End of Servicing" -Value $EOS
+          $data += $obj  
+        }
+        $data2 += $data
+      }
       
-      if ($build) { Return $data2 | Where-Object { $_."Os Build" -like "*$build*" } | ConvertTo-Json }
+      if ($Build) { Return $data2 | Where-Object { $_."Os Build" -like "*$Build*" } | ConvertTo-Json }
       if ($KB) { Return $data2 | Where-Object { $_."Kb article" -like "*$KB*" } | ConvertTo-Json }
       if ($Date) { Return $data2 | Where-Object { $_."Availability date" -eq "*$Date*" } | ConvertTo-Json }
       if ($ServiceOption) { Return $data2 | Where-Object { $_."Servicing option" -like "*$ServiceOption*" } | ConvertTo-Json }
@@ -77,9 +75,10 @@ Function Get-WindowsReleaseInformationHistory {
         if ($table.ParentNode.SelectNodes('strong').InnerText)
         { $ver = ($table.ParentNode.SelectNodes('strong').InnerText).Split(' ')[1] }
         else {
-          $Ver = ($table.ParentNode.FirstChild.InnerText).Split(' ')[1]}
-          if ($ver -like "*End of servicing*") { $EOS = $true }
-          else { $EOS = $False }
+          $Ver = ($table.ParentNode.FirstChild.InnerText).Split(' ')[1]
+        }
+        if ($ver -like "*End of servicing*") { $EOS = $true }
+        else { $EOS = $False }
         $data = @()
         foreach ($tablerow in ($table.Elements('tr') | Select-Object -Skip 1)) {
 
@@ -98,7 +97,7 @@ Function Get-WindowsReleaseInformationHistory {
         $data2 += $data
       }
 
-      if ($build) { Return $data2 | Where-Object { $_."Os Build" -like "*$build*" } | ConvertTo-Json }
+      if ($Build) { Return $data2 | Where-Object { $_."Os Build" -like "*$Build*" } | ConvertTo-Json }
       if ($KB) { Return $data2 | Where-Object { $_."Kb article" -like "*$KB*" } | ConvertTo-Json }
       if ($Date) { Return $data2 | Where-Object { $_."Availability date" -eq "*$Date*" } | ConvertTo-Json }
       if ($ServiceOption) { Return $data2 | Where-Object { $_."Servicing option" -like "*$ServiceOption*" } | ConvertTo-Json }
